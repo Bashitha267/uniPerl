24b5ac72cf6f8ac6614d16df1df0b0b30309ff56
DCS User <DCSUser@cn.lan>
2025-07-16 08:29:55 +0530

    Edited

--- arr.pl
+++ arr.pl
@@ -1,2 +1,9 @@
 @colors=("#0000","#1111","green","Red");
-print"@colors[2..4]";
+print"@colors[2..4]\n";
+
+@marks=(23,34,56,88,87,46);
+splice(@marks,2,3,97..100);
+print"@marks\n";
+
+splice(@colors,1,2,"Blue","Yellow");
+print"@colors";
