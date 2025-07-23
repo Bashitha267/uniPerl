a5f9864518e58ebdecea5104fb3c2f3a21573189
DCS User <DCSUser@cn.lan>
2025-07-23 13:46:23 +0530

    Added;

---
+++ July23/Dictionary.pl
@@ -0,0 +1,20 @@
+%Experience=("perl"=>5,"Java"=>1,"python"=>2);
+print $Experience{"perl"};
+print"\n";
+@keyArray=keys%Experience;
+print "Size of the array:".scalar@keyArray."\n";
+if(exists($Experience{"perl"})){
+	print"exists";
+}
+else {
+	print"Not Exists";
+}
+print"\n";
+@ExperienceValues=values%Experience;
+print scalar@Experiencevalues;
+
+$Experience{"oracle"}=2;
+$Experience{"Java Script"}=3;
+print"\n";
+print %Experience;
+
